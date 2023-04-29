09d2ecb638cf8466a2ecab23b77acae859923076
jaidaneachraf <80226456+jaidaneachraf@users.noreply.github.com>
2023-04-29 17:12:23 +0200

    List and add reclamation

---
+++ src/com/pidev/MyApplication.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pidev;
+
+import com.codename1.io.Log;
+import static com.codename1.ui.CN.addNetworkErrorListener;
+import static com.codename1.ui.CN.getCurrentForm;
+import static com.codename1.ui.CN.updateNetworkThreadCount;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Form;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import com.mycompany.gui.AllReclamation;
+
+/**
+ *
+ * @author achra
+ */
+public class MyApplication {
+
+    private Form current;
+    private Resources theme;
+
+    public void init(Object context) {
+        // use two network threads instead of one
+        updateNetworkThreadCount(2);
+
+        theme = UIManager.initFirstTheme("/theme");
+
+        // Enable Toolbar on all Forms by default
+        Toolbar.setGlobalToolbar(true);
+
+        // Pro only feature
+        Log.bindCrashProtection(true);
+
+        addNetworkErrorListener(err -> {
+            // prevent the event from propagating
+            err.consume();
+            if(err.getError() != null) {
+                Log.e(err.getError());
+            }
+            Log.sendLogAsync();
+            Dialog.show("Connection Error", "There was a networking error in the connection to " + err.getConnectionRequest().getUrl(), "OK", null);
+        });        
+    }
+    
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+      new AllReclamation(theme).show();
+    }
+
+    public void stop() {
+        current = getCurrentForm();
+        if(current instanceof Dialog) {
+            ((Dialog)current).dispose();
+            current = getCurrentForm();
+        }
+    }
+    
+    public void destroy() {
+    }
+
+}
